596f4529ecc09742e96d764c19681b5ee4d4b663
David garrick <d.garrick@live.com>
2022-05-11 13:08:26 +0100

    update deploy file

--- deploy.sh
+++ deploy.sh
@@ -9,18 +9,12 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-
 git init
 git checkout -b main
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-<<<<<<< HEAD
 git push -f git@github.com:DavidGarr-Bang/DavidGarr-Bang.github.io.git main
-=======
-git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
->>>>>>> 48cd620ee19e7f6892f0727bd323da5fa7b18fbf
-
 
 cd -
