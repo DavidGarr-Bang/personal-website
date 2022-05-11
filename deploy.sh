3ce74b4f0eb82a992f5f00290ae2fe3cf340e4cd
David garrick <d.garrick@live.com>
2022-05-11 12:57:23 +0100

    try and deploy

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+git push -f git@github.com:DavidGarr-Bang/DavidGarr-Bang.github.io.git main
 
 
 cd -
